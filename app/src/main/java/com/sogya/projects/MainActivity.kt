87b4d665fee3a141b6eba9277670fc4e7fd1a593
SogYa <Ghost.Spartan36@gmail.com>
2022-04-18 22:12:13 +0300

    added processing text changes

--- app/src/main/java/com/sogya/projects/MainActivity.kt
+++ app/src/main/java/com/sogya/projects/MainActivity.kt
@@ -2,22 +2,30 @@ package com.sogya.projects
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.sogya.projects.instruments.OnDataPass
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnDataPass {
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
+    private lateinit var topBarTittle: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        settupNavigation()
+        topBarTittle = findViewById(R.id.topBarTittle)
+        setupNavigation()
     }
 
-    private fun settupNavigation() {
+    private fun setupNavigation() {
         navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
+    }
+
+    override fun onDataPass(data: String) {
+        topBarTittle.text = data
     }
 
     override fun onBackPressed() {
