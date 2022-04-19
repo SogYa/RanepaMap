0f1aaff7f26f0f2d4c044abb262000f8839883ae
SogYa <Ghost.Spartan36@gmail.com>
2022-04-19 19:39:27 +0300

    added constants object to instruments

---
+++ app/src/main/java/com/sogya/projects/instruments/Constants.kt
@@ -0,0 +1,8 @@
+package com.sogya.projects.instruments
+
+import com.sogya.projects.R
+
+object Constants {
+    const val MINIMAL_FLOOR_NUMBER = 1
+    const val DEFAULT_RESORCE_FOR_MAP_FRAGMENT = R.drawable.ic_launcher_foreground
+}
