e2025433b8a3dd26e9eca3feeab78a13011b9f9d
SogYa <Ghost.Spartan36@gmail.com>
2022-04-19 19:44:20 +0300

    added methods that works with floors

--- app/src/main/java/com/sogya/projects/screens/mapscreen/MapFragment.kt
+++ app/src/main/java/com/sogya/projects/screens/mapscreen/MapFragment.kt
@@ -5,17 +5,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.setFragmentResultListener
 import com.sogya.projects.R
 import com.sogya.projects.databinding.FragmentMapBinding
+import com.sogya.projects.instruments.Constants
 import com.sogya.projects.instruments.OnDataPass
 import com.sogya.projects.models.Building
 
 class MapFragment : Fragment(R.layout.fragment_map) {
     private lateinit var binding: FragmentMapBinding
     private lateinit var mOnDataPass: OnDataPass
-    private lateinit var selectedBuilding: String
+    private lateinit var selectedBuilding: Building
+    private var floorCounter = Constants.MINIMAL_FLOOR_NUMBER
+    private var floorResource: Int = Constants.DEFAULT_RESORCE_FOR_MAP_FRAGMENT
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,12 +31,58 @@ class MapFragment : Fragment(R.layout.fragment_map) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        selectedBuilding = arguments?.get("building").toString()
-        mOnDataPass.onDataPass(selectedBuilding)
+        selectedBuilding = arguments?.getSerializable("building") as Building
+        mOnDataPass.onDataPass(selectedBuilding.label)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setFloor()
+        binding.photoView.setImageResource(setFloorResource())
+        binding.textViewFloorNumber.text = floorCounter.toString()
+
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         mOnDataPass = context as OnDataPass
+    }
+
+    private fun setFloor() {
+
+        binding.buttonDown.setOnClickListener {
+            if (floorCounter > Constants.MINIMAL_FLOOR_NUMBER) {
+                floorCounter--
+            } else {
+                Toast.makeText(
+                    context,
+                    getString(R.string.toast_floor_down_attention),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        binding.buttonUp.setOnClickListener {
+            if (floorCounter < selectedBuilding.floorNumber) {
+                floorCounter++
+            } else {
+                Toast.makeText(
+                    context,
+                    getString(R.string.toast_floor_up_attention),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun setFloorResource(): Int {
+        when (floorCounter) {
+            1 -> floorResource = selectedBuilding.buildingsFloorsEnums.firstFloorResource
+            2 -> floorResource = selectedBuilding.buildingsFloorsEnums.secondFloorResource
+            3 -> floorResource = selectedBuilding.buildingsFloorsEnums.thirdFloorResource!!
+            4 -> floorResource = selectedBuilding.buildingsFloorsEnums.fourFloorResource!!
+            5 -> floorResource = selectedBuilding.buildingsFloorsEnums.fiveFloorResource!!
+            6 -> floorResource = selectedBuilding.buildingsFloorsEnums.sixFloorResource!!
+        }
+        return floorResource
     }
 }
