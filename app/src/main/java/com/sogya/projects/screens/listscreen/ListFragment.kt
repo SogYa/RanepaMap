247833083471a180bfc0c379c8857310763a2022
SogYa <Ghost.Spartan36@gmail.com>
2022-04-20 17:19:55 +0300

    added viewmodel realization and deleted useless buildings list

--- app/src/main/java/com/sogya/projects/screens/listscreen/ListFragment.kt
+++ app/src/main/java/com/sogya/projects/screens/listscreen/ListFragment.kt
@@ -7,19 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.sogya.projects.R
 import com.sogya.projects.databinding.FragmentListBinding
-import com.sogya.projects.instruments.BuildingsFloorsEnums
 import com.sogya.projects.instruments.OnDataPass
 import com.sogya.projects.models.Building
-import java.io.Serializable
-
 
 class ListFragment : Fragment(R.layout.fragment_list), ListAdapter.OnBuildingClickListener {
     private lateinit var binding: FragmentListBinding
-    private var buildingsList: ArrayList<Building> = ArrayList()
     private lateinit var adapter: ListAdapter
     private lateinit var mOnDataPass: OnDataPass
 
@@ -34,58 +31,18 @@ class ListFragment : Fragment(R.layout.fragment_list), ListAdapter.OnBuildingCli
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val vm: ListVM = ViewModelProvider(this).get(ListVM::class.java)
+
         val layoutManager = GridLayoutManager(context, 2)
         binding.buildingsRecyclerView.layoutManager = layoutManager
         adapter = ListAdapter(this)
         binding.buildingsRecyclerView.adapter = adapter
-        if (buildingsList.isEmpty()) {
-            buildingsList.add(
-                Building(
-                    R.drawable.building1,
-                    "Первый корпус",
-                    3,
-                    BuildingsFloorsEnums.FIRST
-                )
-            )
-            buildingsList.add(
-                Building(
-                    R.drawable.korpus2,
-                    "Второй корпус",
-                    2,
-                    BuildingsFloorsEnums.SECOND
-                )
-            )
-            buildingsList.add(
-                Building(
-                    R.drawable.korpus3,
-                    "Третий корпус",
-                    4,
-                    BuildingsFloorsEnums.THIRD
-                )
-            )
-            buildingsList.add(
-                Building(
-                    R.drawable.korpus5,
-                    "Пятый корпус",
-                    6,
-                    BuildingsFloorsEnums.FIVE
-                )
-            )
-            buildingsList.add(
-                Building(
-                    R.drawable.korpus6,
-                    "Шестой корпус",
-                    3,
-                    BuildingsFloorsEnums.SIX
-                )
-            )
-        }
-    }
 
-    override fun onStart() {
-        super.onStart()
-        adapter.updateBuildingList(buildingsList)
-        mOnDataPass.onDataPass("Главная")
+        vm.buildingListLiveData.observe(viewLifecycleOwner) {
+            adapter.updateBuildingList(it)
+        }
+
+        mOnDataPass.onDataPass(getString(R.string.main_title))
     }
 
     override fun onAttach(context: Context) {
@@ -94,11 +51,9 @@ class ListFragment : Fragment(R.layout.fragment_list), ListAdapter.OnBuildingCli
     }
 
     override fun onClick(building: Building) {
-
         findNavController().navigate(
-            R.id.action_listFragment_to_mapFragment, bundleOf("building" to building)
+            R.id.action_listFragment_to_mapFragment,
+            bundleOf("buildingId" to building.buildingId)
         )
-
     }
-
 }
--- app/src/main/java/com/sogya/projects/screens/listscreen/ListVM.kt
+++ app/src/main/java/com/sogya/projects/screens/listscreen/ListVM.kt
@@ -1,4 +1,15 @@
 package com.sogya.projects.screens.listscreen
 
-class ListVM {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sogya.projects.models.Building
+import com.sogya.projects.models.InMemoryBuildingsRepository
+
+class ListVM : ViewModel() {
+    var buildingListLiveData: MutableLiveData<ArrayList<Building>> = MutableLiveData()
+    private val inMemoryBuildingsRepository = InMemoryBuildingsRepository
+
+    init {
+        buildingListLiveData.postValue(inMemoryBuildingsRepository.getInstance().getList())
+    }
 }
