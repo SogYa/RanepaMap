ef9b8e1cff5e25e76968895d2a5e67910d5967f8
SogYa <Ghost.Spartan36@gmail.com>
2022-04-21 22:10:32 +0300

    added new constant

--- app/src/main/java/com/sogya/projects/instruments/Constants.kt
+++ app/src/main/java/com/sogya/projects/instruments/Constants.kt
@@ -4,5 +4,6 @@ import com.sogya.projects.R
 
 object Constants {
     const val MINIMAL_FLOOR_NUMBER = 1
-    const val DEFAULT_RESORCE_FOR_MAP_FRAGMENT = R.drawable.ic_launcher_foreground
+    const val DEFAULT_RESOuRCE_FOR_MAP_FRAGMENT = R.drawable.ic_launcher_foreground
+    const val NIGHT_MODE = "NIGHT_MODE"
 }
