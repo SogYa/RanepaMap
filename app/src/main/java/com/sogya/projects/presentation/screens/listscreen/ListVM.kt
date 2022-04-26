42c94c7547d33966ca9344d19d4643826ecb5fa9
SogYa <Ghost.Spartan36@gmail.com>
2022-04-26 14:39:32 +0300

    changed get list method

---
+++ app/src/main/java/com/sogya/projects/presentation/screens/listscreen/ListVM.kt
@@ -0,0 +1,18 @@
+package com.sogya.projects.presentation.screens.listscreen
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sogya.projects.data.BuildingsRepositoryImpl
+import com.sogya.projects.domain.GetBuildingsListUseCase
+import com.sogya.projects.models.Building
+
+class ListVM : ViewModel() {
+    var buildingListLiveData: MutableLiveData<ArrayList<Building>> = MutableLiveData()
+    private val repository = BuildingsRepositoryImpl
+    private val getBuildingsListUseCase =
+        GetBuildingsListUseCase(repository.getInstance())
+
+    init {
+        buildingListLiveData.postValue(getBuildingsListUseCase.getBuildingsList())
+    }
+}
