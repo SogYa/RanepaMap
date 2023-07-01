c3b78bc6dff845bc4a0dccbb61d1df14f40ffecc
SogYa <Ghost.Spartan36@gmail.com>
2023-07-01 15:31:08 +0300

    chore

--- app/src/main/java/com/sogya/projects/instruments/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.sogya.projects.instruments
-
-import com.sogya.projects.R
-
-object Constants {
-    const val MINIMAL_FLOOR_NUMBER = 1
-    const val DEFAULT_RESOURCE_FOR_MAP_FRAGMENT = R.drawable.ic_launcher_foreground
-    const val NIGHT_MODE = "NIGHT_MODE"
-}
