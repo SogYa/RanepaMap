3b11fd587c49075aab7e9a36057dec0277f642ef
SogYa <Ghost.Spartan36@gmail.com>
2023-07-09 16:46:34 +0300

    added imageId field

--- app/src/main/java/com/sogya/projects/models/BuildingPresentation.kt
+++ app/src/main/java/com/sogya/projects/models/BuildingPresentation.kt
@@ -4,7 +4,7 @@ import ru.sogya.projects.domain.models.BuildingDomain
 
 data class BuildingPresentation(
     override val buildingId: Int,
-    override val imageUri: String,
+    var imageId: Int,
     override val label: String,
     override val floorsList: List<FloorPresentation>
 ) : BuildingDomain
--- app/src/main/java/com/sogya/projects/models/FloorPresentation.kt
+++ app/src/main/java/com/sogya/projects/models/FloorPresentation.kt
@@ -3,6 +3,7 @@ package com.sogya.projects.models
 import ru.sogya.projects.domain.models.FloorDomain
 
 data class FloorPresentation(
-    override val imageUri: String,
-    override val floorNumber: Int
+    override val imageId: Int,
+    var drawableId: Int?,
+    override var floorNumber: Int
 ) : FloorDomain
