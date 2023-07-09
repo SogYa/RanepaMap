bb2b3981625519fecc50a33110bd41470aff5cd4
SogYa <Ghost.Spartan36@gmail.com>
2023-07-09 16:47:06 +0300

    added get resource arrays functions

--- app/src/main/java/com/sogya/projects/app/App.kt
+++ app/src/main/java/com/sogya/projects/app/App.kt
@@ -1,8 +1,27 @@
 package com.sogya.projects.app
 
 import android.app.Application
+import android.content.res.TypedArray
+import com.sogya.projects.R
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App :Application(){
+class App : Application() {
+    companion object {
+        private var floorDrawableArray: TypedArray? = null
+        private var buildingDrawableArray: TypedArray? = null
+        fun getFloorDrawableArray(): TypedArray? {
+            return floorDrawableArray
+        }
+
+        fun getBuildingDrawableArray(): TypedArray? {
+            return buildingDrawableArray
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        floorDrawableArray = resources.obtainTypedArray(R.array.floors)
+        buildingDrawableArray = resources.obtainTypedArray(R.array.buildings)
+    }
 }
