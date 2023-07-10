a2d1d2576dc058f6aa80e4f37f271bece1913f1f
SogYa <Ghost.Spartan36@gmail.com>
2023-07-10 17:52:28 +0300

    reworked back pressed function

--- app/src/main/java/com/sogya/projects/screens/MainActivity.kt
+++ app/src/main/java/com/sogya/projects/screens/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.sogya.projects.screens
 
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.viewModels
+import android.window.OnBackInvokedDispatcher
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -13,14 +15,29 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
-    private val vm: MainVM by viewModels()
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupNavigation()
+
+        if (Build.VERSION.SDK_INT >= 33) {
+            onBackInvokedDispatcher.registerOnBackInvokedCallback(
+                OnBackInvokedDispatcher.PRIORITY_DEFAULT
+            ) {
+                backPressed()
+            }
+        } else {
+            onBackPressedDispatcher.addCallback(this,
+                object : OnBackPressedCallback(true) {
+                    override fun handleOnBackPressed() {
+                        backPressed()
+                    }
+                })
+        }
     }
 
     private fun setupNavigation() {
@@ -34,11 +51,7 @@ class MainActivity : AppCompatActivity() {
         _binding = null
     }
 
-    @Deprecated("Deprecated in Java")
-    override fun onBackPressed() {
-        super.onBackPressed()
-        for (i in 0 until supportFragmentManager.backStackEntryCount) {
-            supportFragmentManager.popBackStack()
-        }
+    private fun backPressed() {
+        onBackPressedDispatcher.onBackPressed()
     }
 }
